c8b4829e4f380a22f7ff38db4362b99616c752b6
Lizzie <leejihye7117@gmail.com>
2022-11-21 13:39:29 +0900

    [refactor]router 분리

--- back/src/controllers/PostController.js
+++ back/src/controllers/PostController.js
@@ -1,60 +1,79 @@
-const mongoose = require("mongoose");
-const postService = require("../services/postServiece");
+const express = require("express");
+const Post = require("../db/models/Post");
+const { loginRequired } = require("../middlewares/login_required");
 
-// 전체 게시글 조회
-exports.getAllPosts = async (req, res) => {
-  try {
-    console.log("전체 게시글 조회");
-    const posts = await postService.getAllPosts();
-    res.json({ data: posts, status: "success" });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
+const postController = {
+  // 전체 게시글 조회
+  getAllPosts: async (req, res) => {
+    const { page = "1", limit = "10" } = req.query;
 
-// 게시글 생성
-exports.createPost = async (req, res) => {
-  try {
-    console.log("게시글 생성");
-    const post = await postService.createPost(req.body);
-    res.json({ data: post, status: "success" });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
+    const list = await Post.findAll({
+      page,
+      limit,
+      sort: {
+        createdAt: -1,
+      },
+    });
 
-// 특정 게시글 조회
-exports.getPostById = async (req, res) => {
-  try {
-    console.log("특정 게시글 조회");
+    return res.json(list);
+  },
+
+  // 특정 게시글 조회
+  getPostById: async (req, res) => {
     const { postId } = req.params;
-    const post = await postService.getPostById(postId);
-    res.json({ data: post, status: "success" });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+    const post = await Post.get(postId).populate([
+      { path: "author", select: ["_id", "email", "name"] },
+    ]);
+
+    return res.json(post);
+  },
+
+  // 게시글 작성
+  createPost: async (req, res) => {
+    const post = req.body;
+    post.author = req.currentUserId;
+
+    const newPost = await Post.create(post);
+
+    return res.json({
+      id: newPost.id,
+    });
+  },
+
+  // 게시글 수정
+  updatePost: async (req, res, next) => {
+    const post = req.body;
+    const { postId } = req.params;
+
+    const getPost = await Post.get(postId);
+
+    if (getPost.author !== req.currentUserId) {
+      return res.status(401).json({
+        message: "수정 권한이 없습니다.",
+      });
+    }
+
+    let result = null;
+
+    try {
+      result = await Post.update(post);
+    } catch (err) {
+      return next(err.message);
+    }
+
+    return res.json(result);
+  },
+
+  // 게시글 삭제
+  deletePost: async (req, res) => {
+    const { postId } = req.params;
+
+    await Post.delete(postId);
+
+    return res.json({
+      id: postId,
+    });
+  },
 };
 
-// 게시글 수정
-exports.updatePost = async (req, res) => {
-  try {
-    console.log("게시글 수정");
-    const { postId } = req.params;
-    const post = await postService.updatePost(postId, req.body);
-    res.json({ data: post, status: "success" });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
-// 게시글 삭제
-exports.deletePost = async (req, res) => {
-  try {
-    console.log("게시글 삭제");
-    const { postId } = req.params;
-    const post = await postService.deletePost(postId);
-    res.json({ data: post, status: "success" });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
+exports.postController = postController;
--- back/src/routers/PostRouter.js
+++ back/src/routers/PostRouter.js
@@ -1,80 +1,33 @@
 const express = require("express");
 const Post = require("../db/models/Post");
 const { loginRequired } = require("../middlewares/login_required");
+const { postController } = require("../controllers/postController");
 
 const postRouter = express.Router();
 
 /**
  * GET /posts
  */
-postRouter.get("/", async (req, res) => {
-  const { page = "1", limit = "10" } = req.query;
-
-  const list = await Post.findAll({
-    page,
-    limit,
-    sort: {
-      createdAt: -1,
-    },
-  });
-
-  return res.json(list);
-});
+postRouter.get("/", postController.getAllPosts);
 
 /**
  * GET /posts/:postId
  */
-postRouter.get("/:postId", async (req, res) => {
-  const { postId } = req.params;
-  const post = await Post.get(postId).populate([
-    { path: "author", select: ["_id", "email", "name"] },
-  ]);
+postRouter.get("/:postId", postController.getPostById);
 
-  return res.json(post);
-});
+/**
+ * POST /posts
+ */
+postRouter.post("/", [loginRequired], postController.createPost);
 
-postRouter.put("/:postId", [loginRequired], async (req, res, next) => {
-  const post = req.body;
-  const { postId } = req.params;
+/**
+ * POST /posts/:postId
+ */
+postRouter.put("/:postId", [loginRequired], postController.updatePost);
 
-  const getPost = await Post.get(postId);
-
-  if (getPost.author !== req.currentUserId) {
-    return res.status(401).json({
-      message: "수정 권한이 없습니다.",
-    });
-  }
-
-  let result = null;
-  try {
-    result = await Post.update(post);
-  } catch (e) {
-    console.log(JSON.stringify(e));
-    return next(e.message);
-  }
-
-  return res.json(result);
-});
-
-postRouter.post("/", [loginRequired], async (req, res) => {
-  const post = req.body;
-  post.author = req.currentUserId;
-
-  const newPost = await Post.create(post);
-
-  return res.json({
-    id: newPost.id,
-  });
-});
-
-postRouter.delete("/:postId", [loginRequired], async (req, res) => {
-  const { postId } = req.params;
-
-  await Post.delete(postId);
-
-  return res.json({
-    id: postId,
-  });
-});
+/**
+ * DELETE /posts/:postId
+ */
+postRouter.delete("/:postId", [loginRequired], postController.deletePost);
 
 module.exports = postRouter;
