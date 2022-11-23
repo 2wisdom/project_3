c76c1704a362bdc95db5735448837788b8d08434
pakeunjung <scpark6021@gmail.com>
2022-11-23 20:16:02 +0900

    [design] 회원가입, 로그인 ui

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Login from "./components/login"
-import Register from "./components/Register"
-import Info from "./pages/InfoPage"
+import NavBar from '@/components/NavBar';
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Info from "./pages/InfoPage";
 
 const App = () => {
     return (
         <>
             <BrowserRouter>
+                <NavBar></NavBar>
                 <Routes>
                     <Route path="/" element={<Info />} />
                     <Route path="/login" element={<Login />} />
