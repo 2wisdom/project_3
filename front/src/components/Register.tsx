9943ca563b4cfd6a88d087e89137dc2fb9a07d7d
pakeunjung <scpark6021@gmail.com>
2022-11-23 10:42:12 +0900

    [feat]로그인, 회원가입 구현중

--- front/src/components/Register.tsx
+++ front/src/components/Register.tsx
@@ -1,3 +1,4 @@
+// import "./styles.css";
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Link, useMatch, useNavigate } from "react-router-dom";
@@ -5,8 +6,39 @@ import { Link, useMatch, useNavigate } from "react-router-dom";
 const Register = () => {
   return (
     <MainContent>
-      <LoginContainer>
-        <LoginTitle>회원가입</LoginTitle>
+      <RegisterContainer>
+        <Title>회원가입</Title>
+        <Tag>닉네임</Tag>
+        <NicknameInput
+          id="email"
+          type="text"
+          placeholder="아이디를 입력하세요"
+          // value={userData.email}
+          // onChange={(e) => setUserData({ ...userData, email: e.target.value })}
+        />
+        <NinameConfirmBtn>중복 확인</NinameConfirmBtn>
+        <PasswordInput
+          id="password"
+          type="text"
+          placeholder="비밀번호를 입력하세요"
+          // value={userData.email}
+          // onChange={(e) => setUserData({ ...userData, email: e.target.value })}
+        />
+        {/* 닉네임
+      <input
+        placeholder="닉네임을 입력하세요"></input>
+      <button>중복확인</button>
+      이메일
+      <input></input>
+      <button>중복확인</button>
+      비밀번호
+      <input>
+      </input>
+      비밀번호 확인
+      <input>
+      </input>
+      <button>회원가입</button> */}
+        {/* <LoginTitle>회원가입</LoginTitle>
         <LoginInput id="email" type="text" placeholder="아이디를 입력하세요" />
         <LoginInput
           id="password"
@@ -14,28 +46,35 @@ const Register = () => {
           placeholder="비밀번호를 입력하세요"
         />
         <LoginButton>회원가입</LoginButton>
-      </LoginContainer>
+      </LoginContainer> */}
+      </RegisterContainer>
     </MainContent>
   );
 };
 export default Register;
 
 const MainContent = styled.div`
-  width: 70%;
-  height: 80%;
+  width: 70vw;
+  height: 80vh;
   margin: 0 auto;
 `;
-const P = styled.div`
-  margin-top: 5%;
-  margin-bottom: 5%;
-`;
-const LoginContainer = styled.form`
-  width: 100%;
-  height: 80%;
-  flex-direction: column;
-  justify-content: center;
-  align-item: center;
-  text-align: center;
+
+const RegisterContainer = styled.form`
+  width: 70vw;
+  height: 80vh;
+  display: grid;
+  background-color: yellow;
+  grid-template-columns: 1fr 650px 1fr;
+  // grid-template-rows: repeat(5, 1fr) 2fr;
+  grid-template-areas:
+    ". title ."
+    "tag input confirm"
+    "tag input confirm"
+    "tag input ."
+    "tag input ."
+    ". btn .";
+  gap: 0.25rem;
+  place-items: center;
   font-family: "NanumGothic";
   font-style: normal;
   font-weight: 400;
@@ -43,7 +82,10 @@ const LoginContainer = styled.form`
   line-height: 20px;
 `;
 
-const LoginTitle = styled.div`
+const Title = styled.div`
+  background-color: blue;
+  grid-area: title;
+  border: 2px solid pink;
   font-weight: 700;
   font-size: 36px;
   line-height: 36px;
@@ -53,9 +95,21 @@ const LoginTitle = styled.div`
   text-underline-position: under;
 `;
 
-const LoginInput = styled.input`
+const Tag = styled.div`
+  grid-area: tag;
+  font-family: "NanumGothic";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 24px;
+  text-align: center;
+`;
+
+const NicknameInput = styled.input`
+  grid-area: input;
   width: 470px;
   height: 74px;
+  grid-area: input
   display: block;
   background: #ebebeb;
   border-radius: 12px;
@@ -72,19 +126,96 @@ const LoginInput = styled.input`
   }
 `;
 
-const LoginButton = styled.button`
-  width: 470px;
-  height: 92px;
-  color: #ffffff;
-  background: #000000;
-  border-radius: 12px;
-  font-weight: 700;
-  font-size: 36px;
-  line-height: 36px;
-  margin: 0 auto;
-  margin-top: 5%;
-  cursor: pointer;
+const NinameConfirmBtn = styled.button`
+  grid-area: confirm;
+  width: 85px;
+  height: 28px;
+
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 23px;
+  line-height: 28px;
+  /* identical to box height */
+
+  display: flex;
+  align-items: center;
+
+  color: #3278e4;
 `;
+const PasswordInput = styled(NicknameInput)``;
+
+// .item: title {
+//   grid-column: 1/4;
+//   grid-row: 1/2;
+//   background-color: #blue;
+
+// }
+// const MainContent = styled.div`
+//   width: 70%;
+//   height: 80%;
+//   margin: 0 auto;
+// `;
+// const P = styled.div`
+//   margin-top: 5%;
+//   margin-bottom: 5%;
+// `;
+// const LoginContainer = styled.form`
+//   width: 100%;
+//   height: 80%;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-item: center;
+//   text-align: center;
+//   font-family: "NanumGothic";
+//   font-style: normal;
+//   font-weight: 400;
+//   font-size: 20px;
+//   line-height: 20px;
+// `;
+
+// const LoginTitle = styled.div`
+//   font-weight: 700;
+//   font-size: 36px;
+//   line-height: 36px;
+//   margin-bottom: 3%;
+//   margin-top: 8%;
+//   text-decoration: underline solid #3278e4;
+//   text-underline-position: under;
+// `;
+
+// const LoginInput = styled.input`
+//   width: 470px;
+//   height: 74px;
+//   display: block;
+//   background: #ebebeb;
+//   border-radius: 12px;
+//   border: none;
+//   display: block;
+//   margin: 0 auto;
+//   margin-top: 3%;
+//   margin-bottom: 3%;
+//   text-align: center;
+//   font-size: 24px;
+//   line-height: 24px;
+//   :focus {
+//     border: 3px solid #3278e4;
+//   }
+// `;
+
+// const LoginButton = styled.button`
+//   width: 470px;
+//   height: 92px;
+//   color: #ffffff;
+//   background: #000000;
+//   border-radius: 12px;
+//   font-weight: 700;
+//   font-size: 36px;
+//   line-height: 36px;
+//   margin: 0 auto;
+//   margin-top: 5%;
+//   cursor: pointer;
+// `;
 
 // interface LoginData {
 //     email: string;
