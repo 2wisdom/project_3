9d14d721cd618fec718274a1948b0cf58815cef6
david <p33a33@gmail.com>
2022-11-23 23:08:15 +0900

    feat: 인포페이지작업

---
+++ front/src/components/mainpage/MainPage2.tsx
@@ -0,0 +1,19 @@
+import InfoPageRoot from "../../styles/MainPage/InfoPageRoot";
+import InfoPageContainer from "../../styles/MainPage/InfoPageContainer";
+
+
+const MainPage2 = () => {
+  return (
+    <>
+     <InfoPageRoot>
+      <InfoPageContainer>
+      <div className="MainPage2">
+        <p>메인페이지2</p>
+      </div>
+      </InfoPageContainer>
+      </InfoPageRoot>
+    </>
+  );
+};
+
+export default MainPage2;
---
+++ front/src/styles/MainPage/InfoPageContainer.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const InfoPageContainer = styled.div`
+background-color:red;
+background-size: cover;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+color: #FFFFFF;
+height: 90vh;
+font-size: 4rem;
+font-weight: 500;
+width: 100%;
+`
+
+export default InfoPageContainer;
