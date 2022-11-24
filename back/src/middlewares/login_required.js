e0ea8940e59bc7911b85a77d83883c9da10ac038
Lizzie <leejihye7117@gmail.com>
2022-11-24 23:48:28 +0900

    [refactor]전역/필수 미들웨어 분리

--- back/src/middlewares/login_required.js
+++ back/src/middlewares/login_required.js
@@ -1,35 +1,16 @@
-const jwt = require("jsonwebtoken");
-const { default: mongoose } = require("mongoose");
-
+/**
+ * 필수 인증 미들웨어
+ *
+ * 반드시 인증(authentication) 이 필요한 라우터에만
+ * 등록해서 사용
+ */
 function loginRequired(req, res, next) {
-  // request 헤더로부터 authorization bearer 토큰을 받음. authorization: 'Bearer 토큰'
-  const userToken = req.headers["authorization"]?.split(" ")[1] ?? "null";
-
-  // 이 토큰은 jwt 토큰 문자열이거나, 혹은 "null" 문자열임. 토큰이 "null" 일 경우, login_required 가 필요한 서비스 사용을 제한함.
-  if (userToken === "null") {
-    console.log("서비스 사용 요청이 있습니다.하지만, Authorization 토큰: 없음");
+  if (!req.currentUserId) {
     res.status(400).send("로그인한 유저만 사용할 수 있는 서비스입니다.");
     return;
   }
 
-  // 해당 token 이 정상적인 token인지 확인 -> 토큰에 담긴 user_id 정보 추출
-  try {
-    // .env 에서 jwt 서명 가져오기
-    const secretKey = process.env.JWT_SECRET_KEY || "secret-key";
-
-    // 토큰을 서명으로 확인, 디코딩
-    const jwtDecoded = jwt.verify(userToken, secretKey);
-
-    const user_id = jwtDecoded.userId;
-    req.currentUserId = user_id;
-
-    // console.log("req.currentUserId", req.currentUserId);
-
-    next();
-  } catch (error) {
-    res.status(400).send("정상적인 토큰이 아닙니다. 다시 한 번 확인해 주세요.");
-    return;
-  }
+  next();
 }
 
 exports.loginRequired = loginRequired;
