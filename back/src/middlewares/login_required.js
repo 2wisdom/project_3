b85eb1f0ddc9ba67f9e8559d15b6985356e8d173
zimablue14 <zimablue14@SungwooKims-MacBook-Air.local>
2022-11-30 11:55:47 +0900

    [fix]유저 조회 에러 수정

--- back/src/middlewares/login_required.js
+++ back/src/middlewares/login_required.js
@@ -6,7 +6,6 @@ const { User } = require("../db/models/User");
 const { deleteUserImage } = require("../middlewares/deleteImage");
 
 async function loginRequired(req, res, next) {
-
   // request 헤더로부터 authorization bearer 토큰을 받음. authorization: 'Bearer 토큰'
   const userToken = req.headers["authorization"]?.split(" ")[1] ?? "null";
 
@@ -30,7 +29,7 @@ async function loginRequired(req, res, next) {
 
     next();
   } catch (error) {
-    if (error.name === "TokenExpiredError") {
+    if (error.name === "TokenExpiredError" && req.file) {
       await deleteUserImage(req.file.path);
       res.status(403).send("access token expired");
       return;
