237c19f27ca5e144a682d4d68221c4520fbde505
Lizzie <leejihye7117@gmail.com>
2022-12-01 23:08:00 +0900

    [comment]post 안쓰는 코드 주석 제거

--- back/src/controllers/PostController.js
+++ back/src/controllers/PostController.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const Post = require("../db/models/Post");
-// const os = require("os");
 
 const postController = {
   // 전체 게시글 조회
@@ -47,41 +46,6 @@ const postController = {
       return res.status(400).send("Error");
     }
   },
-
-  // // 이미지 업로드
-  // uploadImage: async (req, res) => {
-  //   console.log("이미지 업로드");
-
-  //   if (!req.headers["content-type"].startsWith("multipart/form-data")) {
-  //     throw Error({ message: "Content-Type once multipart/form-data" });
-  //   }
-
-  //   const isWindow = os.platform() === "win32";
-
-  //   const url = `${req.protocol}://${req.hostname}${
-  //     parseInt(process.env.SERVER_PORT, 10) === 80
-  //       ? ""
-  //       : `:${process.env.SERVER_PORT}`
-  //   }`;
-
-  //   let path = req.file.path;
-
-  //   // support window
-  //   if (isWindow) {
-  //     path = path.split("\\").join("/");
-  //   }
-
-  //   const resolveUrl = `${url}/${path}`;
-
-  //   try {
-  //     return res.json({
-  //       url: resolveUrl,
-  //     });
-  //   } catch (err) {
-  //     console.log(err);
-  //     return res.status(400).send("Error");
-  //   }
-  // },
 
   // 게시글 생성
   createPost: async (req, res) => {
