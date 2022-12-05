b597f6de57fb68a85efdd3913b6f6388a957e41f
zimablue14 <zimablue14@SungwooKims-MacBook-Air.local>
2022-12-05 18:56:29 +0900

    [refactor]multer 리팩토링

--- back/src/middlewares/validationSchema.js
+++ back/src/middlewares/validationSchema.js
@@ -37,7 +37,10 @@ const validationSchema = {
   }),
   // 비밀번호 수정 유효성 검사 스키마
   putUserSchema: Joi.object({
-    password: password,
+    password: Joi.string()
+      .trim()
+      .regex(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*\W)(?=.\S+$).{8,20}$/)
+      .allow(null, ""),
   }),
 };
 exports.validationSchema = validationSchema;
