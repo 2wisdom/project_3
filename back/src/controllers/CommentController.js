3b20f080361ab37a9b37eca2dbc0bc3fb63d40d1
Lizzie <leejihye7117@gmail.com>
2022-12-08 01:49:12 +0900

    [feat]comment delete 추가

--- back/src/controllers/CommentController.js
+++ back/src/controllers/CommentController.js
@@ -37,8 +37,16 @@ const commentController = {
   },
 
   // 댓글 삭제
-  deleteComment: (req, res) => {
+  deleteComment: async (req, res) => {
     console.log("댓글 삭제");
+
+    const { commentId } = req.params;
+    console.log("id", req.params);
+
+    Comment.findByIdAndDelete(commentId).exec((err) => {
+      if (err) return res.json({ success: false, err });
+      res.status(200).json({ success: true });
+    });
   },
 };
 
