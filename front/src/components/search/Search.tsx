31e86d3961a0a41fe9886e6eb119a042251f0b55
HaEun <rjadmszhd777@naver.com>
2022-12-07 20:15:09 +0900

    [feat]검색구현

--- front/src/components/search/Search.tsx
+++ front/src/components/search/Search.tsx
@@ -3,6 +3,7 @@ import * as SearchStyle from "../../styles/search/SearchStyle";
 import SearchIcon from "@mui/icons-material/Search";
 import { ListItem } from "@mui/material";
 import { SettingsSystemDaydreamTwoTone } from "@mui/icons-material";
+import useDebounce from "@/useDebounce";
 import * as Api from "../../api/Api";
 interface showCard {
   // map: any;
@@ -31,16 +32,17 @@ const Search = ({
   key: string;
   setShowCardData: Dispatch<SetStateAction<showCard[]>>;
 }) => {
-  console.log("key-search", key);
+  // console.log("key-search", key);
   const [searchInput, setSearchInput] = useState<string>("");
   const [page, setPage] = useState<number>(1);
+  const debounceValue = useDebounce(searchInput);
   const [showCards, setShowCards] = useState<showCard[]>([]);
   const [totalPage, setTotalPage] = useState<number>(1);
   const handleOnChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
     setSearchInput(e.target.value);
   };
 
-  console.log("searchInput", searchInput);
+  // console.log("debounceValue", debounceValue);
   // const handleKeyPress: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
   //   if (e.key === "Enter") {
   //     useEffect(() => {
@@ -57,16 +59,21 @@ const Search = ({
   //   }
   // };
   useEffect(() => {
-    if (searchInput) {
-      Api.get(
-        `search/posts?option=all&question=${searchInput}&page=${page}`,
+    const getSearchCards = async () => {
+      return await Api.get(
+        `search/posts?option=all&question=${debounceValue}&page=${page}`,
         null
-      ).then((res) => {
-        setShowCardData(res.data.docs);
-        console.log("search-data", res.data);
-      });
-    }
-  }, [searchInput]);
+      )
+        .then((res) => {
+          setShowCardData(res.data);
+          // console.log("search-data", res.data);
+        })
+        .catch((err) => {
+          console.log("getSearchCards Err", err);
+        });
+    };
+    if (debounceValue) getSearchCards();
+  }, [debounceValue]);
   // console.log()
   return (
     <>
---
+++ front/src/useDebounce.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+function useDebounce(value: string, delay: number = 400) {
+  const [debounceVal, setDebounceVal] = useState<string>(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebounceVal(value);
+    }, delay);
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debounceVal;
+}
+
+export default useDebounce;
