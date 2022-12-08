5056cf5df2574bf6eb4e224ddd5fbe3cf7365dec
박 은정 <scpark6021@gmail.com>
2022-12-08 16:27:46 +0900

    [Feat]: 마켓 get, post 완료

--- front/src/components/buttons/LoginBtn.tsx
+++ front/src/components/buttons/LoginBtn.tsx
@@ -1,13 +1,12 @@
 import LoginBtnStyle from "../../styles/buttons/LoginBtnStyle";
 
-interface LoginProps{
-    isLogin: boolean
+interface LoginProps {
+  isLogin: boolean;
 }
 
-const LoginBtn:React.FunctionComponent<LoginProps> = ({ isLogin }) => {
+const LoginBtn: React.FunctionComponent<LoginProps> = ({ isLogin }) => {
   const btnState = isLogin ? "로그아웃" : "로그인";
 
-  
   return (
     <>
       <LoginBtnStyle className="loginBtn">{btnState}</LoginBtnStyle>
